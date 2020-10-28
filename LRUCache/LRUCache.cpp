b47c125b55a2f93c4234a9faafec0e9b15dd7c58
ofoxxx <oleg.fokin@gmail.com>
2020-10-28 15:57:46 +0200

    HackerRank C++ hard test
    https://www.hackerrank.com/challenges/abstract-classes-polymorphism/problem

--- LRUCache/LRUCache.cpp
+++ LRUCache/LRUCache.cpp
@@ -1,20 +1,138 @@
-// LRUCache.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <vector>
+#include <map>
+#include <string>
+#include <algorithm>
+#include <set>
+#include <cassert>
+using namespace std;
 
-int main()
+struct Node {
+	Node* next;
+	Node* prev;
+	int value;
+	int key;
+	Node(Node* p, Node* n, int k, int val) :prev(p), next(n), key(k), value(val) {};
+	Node(int k, int val) :prev(NULL), next(NULL), key(k), value(val) {};
+};
+
+class Cache {
+
+protected:
+	map<int, Node*> mp; //map the key to the node in the linked list
+	int cp;  //capacity
+	Node* tail; // double linked list tail pointer
+	Node* head; // double linked list head pointer
+	virtual void set(int, int) = 0; //set function
+	virtual int get(int) = 0; //get function
+
+};
+class LRUCache : public Cache
 {
-    std::cout << "Hello World!\n";
+private:
+	void addFirst(Node* n)
+	{
+		if (n == nullptr || n == head)
+			return;
+		if (head == nullptr)
+		{
+			head = tail = n;
+			return;
+		}
+		n->next = head;
+		head->prev = n;
+		head = n;
+	}
+	Node* exclude(Node* n)
+	{
+		if (n == nullptr)
+			return nullptr;
+
+		if (n->prev != nullptr)
+			n->prev->next = n->next;
+		if (n->next != nullptr)
+			n->next->prev = n->prev;
+		if (n == head)
+			head = n->next;
+		if (n == tail)
+			tail = n->prev;
+
+		n->next = n->prev = nullptr;
+		return n;
+	}
+public:
+	LRUCache(int capacity)
+	{
+		assert(capacity > 0);
+		cp = capacity;
+		head = tail = nullptr;
+	}
+	virtual ~LRUCache()
+	{
+		Node* n = tail;
+		while (n != nullptr)
+		{
+			tail = n;
+			n = n->prev;
+			delete tail;
+		}
+		head = tail = nullptr;
+		mp.clear();
+		cp = 0;
+	}
+	virtual void set(int key, int value)
+	{
+		assert(key > 0);
+		assert(value != -1);
+		Node* n = nullptr;
+		auto c = mp.find(key);
+		if (c != mp.end())
+		{
+			n = exclude(c->second);
+			n->value = value;
+		}
+		else
+		{
+			if (mp.size() == cp)
+			{
+				mp.erase(tail->key);
+				Node* r = exclude(tail);
+				n = new (r) Node(key, value);
+			}
+			else
+			{
+				n = new Node(key, value);
+			}
+			mp[key] = n;
+		}
+		assert(n != nullptr);
+		addFirst(n);
+	}
+	virtual int get(int key)
+	{
+		auto c = mp.find(key);
+		if (c != mp.end())
+			return c->second->value;
+		return -1;
+	}
+};
+int main() {
+	int n, capacity, i;
+	cin >> n >> capacity;
+	LRUCache l(capacity);
+	for (i = 0; i < n; i++) {
+		string command;
+		cin >> command;
+		if (command == "get") {
+			int key;
+			cin >> key;
+			cout << l.get(key) << endl;
+		}
+		else if (command == "set") {
+			int key, value;
+			cin >> key >> value;
+			l.set(key, value);
+		}
+	}
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
